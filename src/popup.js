0f05e347f9875a864a10419703b7fc5a25fdb398
Wesley McClure <wes.mcclure@gmail.com>
2012-03-20 19:38:21 -0500

    Start styling stories to indicate status and more information

--- src/popup.js
+++ src/popup.js
@@ -7,10 +7,10 @@ var pivotal = {
 		pivotal.projects([]);
 		pivotalApi.loadUser(userLoaded);
 	},
-	search: function(){
+	search: function() {
 		Enumerable.From(pivotal.projects()).ForEach(loadStories);
 	},
-	refreshSearch: function(){
+	refreshSearch: function() {
 		pivotalApi.clearStories();
 		pivotal.search();
 	}
@@ -39,7 +39,7 @@ function loadProject(project) {
 	loadStories(observable);
 }
 
-function loadStories(project){
+function loadStories(project) {
 	project.stories([]);
 	pivotalApi.loadStories(project.id(), pivotal.filter(), storiesLoaded(project));
 }
@@ -47,13 +47,32 @@ function loadStories(project){
 function storiesLoaded(project) {
 	return function(stories) {
 		Enumerable.From(stories).Select(function(s) {
-			return {
-				id: s.id,
-				url: s.url,
-				name: s.name
-			}
+			console.log(s);
+			return new Story(s);
 		}).ForEach(function(s) {
 			project.stories.push(s)
 		});
 	}
+}
+
+function Story(story) {
+	this.id = story.id;
+	this.url = story.url;
+	this.name = story.name;
+	// todo would be nice to have click to search labels
+	this.labels = story.labels ? story.labels : '';
+	// todo would be nice to have owned by abbreviation and click to search
+	this.owned_by = story.owned_by;
+	this.isFeature = story.story_type === "feature";
+	this.isBug = story.story_type === "bug";
+	this.isRelease = story.story_type === "release";
+	this.isChore = story.story_type === "chore";
+	this.current_state = story.current_state;
+	this.isUnstarted = story.current_state === "unstarted";
+	this.isStarted = story.current_state === "started";
+	this.isDelivered = story.current_state === "delivered";
+	this.isFinished = story.current_state === "finished";
+	this.isAccepted = story.current_state === "accepted";
+	this.isRejected = story.current_state === "rejected";
+	this.isUnscheduled = story.current_state === "unscheduled";
 }
